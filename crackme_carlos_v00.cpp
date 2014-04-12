3c6a0e9fe587cb85416a394baee2771c9b69d134
zebus3d <zebus3dream@gmail.com>
2014-04-12 20:41:18 +0200

    Update crackme_carlos_v00.cpp

--- crackme_carlos_v00.cpp
+++ crackme_carlos_v00.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// funciones:
-// #########################################
-
-// el tipo es lo que devuelve:
-bool comprobacion(string comp1, string comp2) {
-    if ( comp1 != comp2 ) return true;
-    return false;
-}
-
-// el tipo es lo que devuelve:
-string invertir(string caracteres) {
-    string resultado;
-    for (int i = caracteres.size() - 1; i >= 0 ; --i) resultado = resultado + caracteres[i];
-    return resultado;
-}
-
+//prototipado
+bool comprobacion(string, string);
+string invertir(string);
 
 // main:
 // ###########################################
@@ -33,4 +20,20 @@ int main() {
     else {
         cout << "No" << endl;
     }
+}
+
+// funciones:
+// #########################################
+
+// el tipo es lo que devuelve:
+bool comprobacion(string comp1, string comp2) {
+    if ( comp1 != comp2 ) return true;
+    return false;
+}
+
+// el tipo es lo que devuelve:
+string invertir(string caracteres) {
+    string resultado;
+    for (int i = caracteres.size() - 1; i >= 0 ; --i) resultado = resultado + caracteres[i];
+    return resultado;
 }
