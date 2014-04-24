c54fceef509721e82e0dac6aaec9a3e79246b577
zebus3d <zebus3dream@gmail.com>
2014-04-25 00:31:10 +0200

    Create crackme_zb_v00.cpp
    mi primer crackme en c++ programado por mi :)

---
+++ crackme_zb_v00.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// prototipado:
+string consulta(void); // consulta del pass al usuario
+string msj(int); // mensajes de respuesta
+bool test(string); // comprobacion del pass
+
+// main:
+int main(){
+    string m = "";
+    //string uinput = consulta();
+
+    if ( test( consulta() ) ){
+        m = msj(1);
+    } else {
+        m = msj(2);
+    }
+    cout << m << endl;
+    return 0;
+}
+
+// funciones:
+string consulta(void){
+    string uinput;
+    cout << "Inserte la clave" << endl;
+    cin >> uinput;
+    return uinput;
+}
+
+string msj(int i){
+    if ( i == 1 ){
+        string m = "Correcto!!";
+        return m;
+    } else {
+        string m = "Incorrecto!!";
+        return m;
+    }
+}
+
+bool test(string i){
+    string sc = "superpass";
+    if ( i == sc ){
+        return true;
+    }
+}
